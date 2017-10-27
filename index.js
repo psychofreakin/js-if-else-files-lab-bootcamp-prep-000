68d22f474c57491886954aa609208568532f7d05
Rajendra Khatri <007rkhatri@gmail.com>
2017-10-27 19:41:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var height = "74"
 
 if (name === "Susan" && height === "74") {
   console.log("You got both: " + name + " " + height);
-} else {
-  
-}
+} 
 // Don't worry about this
 module.exports = { name, height
 }
